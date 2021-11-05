43d359940e11dc6e751694a714f71dce764a34e9
Fernando González Donat <fernando.donat@unosquare.com>
2021-11-05 15:22:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      isError: false,
+    };
+  }
+
+  increment = () => {
+    this.setState({ isError: false, counter: this.state.counter + 1 });
+  };
+
+  decrement = () => {
+    if (this.state.counter === 0) {
+      this.setState({ ...this.state, isError: true });
+    } else {
+      this.setState({ ...this.state, counter: this.state.counter - 1 });
+    }
+  };
+
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <button data-test="increment-button" onClick={this.increment}>
+          Increment counter
+        </button>
+        <span></span>
+        <button data-test="decrement-button" onClick={this.decrement}>
+          Decrement counter
+        </button>
+        {this.state.isError ? (
+          <span data-test="error-message" style={{ color: "red" }}>
+            The counter can't go below zero
+          </span>
+        ) : null}
+        <span></span>
+      </div>
+    );
+  }
 }
 
 export default App;
